c83b30a1f971f7ead72f93b30f2bf771dd8c6ba4
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-31 15:49:21 -0600

    controllers

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,19 @@ var app = angular.module("firstApp", []);
 app.controller("MyFirstController", function($scope){
   $scope.name = "Severus Snape";
 })
+
+app.controller("ExercisesController", function($scope){
+  $scope.FavColor = "Red";
+  $scope.secondsInACentury = 3600*24*365*100;
+  $scope.date = new Date();
+})
+
+app.controller("MadLibs", function($scope){
+  $scope.boyName="";
+  $scope.adjective="";
+  $scope.noun1="";
+  $scope.noun2="";
+  $scope.insect="";
+  $scope.noun3="";
+  $scope.verb="";
+})
